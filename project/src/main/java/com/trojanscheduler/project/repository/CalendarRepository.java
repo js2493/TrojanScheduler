f143c5caa7cf5af873918ca6ed8033decb55bc39
js2493 <108201703+js2493@users.noreply.github.com>
2024-08-27 17:08:13 -0700

    remove duplicate files

--- project/src/main/java/com/trojanscheduler/project/repository/CalendarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.trojanscheduler.project.repository;
-
-import com.trojanscheduler.project.model.Calendar;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CalendarRepository extends JpaRepository<Calendar, Long> {
-
-}
